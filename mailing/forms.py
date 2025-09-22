5a57cacf82af98b3e1dd6563c6d2df5e9d453902
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-09-22 17:50:29 +0400

    add FormStyleMixin and forms

---
+++ mailing/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from .models import Receiver, Mailing, Message
+from .mixins import FormStyleMixin
+
+class ReceiverForm(FormStyleMixin, forms.ModelForm):
+    placeholder_fields = {
+        "email": "Введите email",
+        "name": "Введите ФИО",
+        "comment": "Комментарий (необязательно)",
+    }
+
+    class Meta:
+        model = Receiver
+        fields = "__all__"
+
+class MessageForm(FormStyleMixin, forms.ModelForm):
+    placeholder_fields = {
+        "title": "Введите тему письма",
+        "text": "Введите текст письма",
+    }
+
+class MailingForm(FormStyleMixin, forms.ModelForm):
+    placeholder_fields = {
+        "message": "Выберете сообщение для отправки",
+        "receiver": "Выберете получателей",
+    }
+
+    class Meta:
+        model = Mailing
+        exclude = ["start_time", "finish_time", "status",]
